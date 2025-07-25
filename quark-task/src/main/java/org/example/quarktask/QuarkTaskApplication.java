87d86c48c0e7af29d33c3042543648613455d954
adefaria <alvarodefaria@macro.com.ar>
2025-07-25 07:26:17 -0300

    Project Structure

---
+++ quark-task/src/main/java/org/example/quarktask/QuarkTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.quarktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuarkTaskApplication.class, args);
+    }
+
+}
---
+++ quark-task/src/test/java/org/example/quarktask/QuarkTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quarktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
